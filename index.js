37c96b5ef7cae2b2b45aeebb5db1cb7a26c4c68a
rsshonjoydas <rsshonjoydas@gmail.com>
2022-05-26 09:52:14 +0600

    Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const CAKE_ORDER = "CAKE_ORDER";
 
 function orderCake() {
@@ -22,3 +25,16 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("update state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
